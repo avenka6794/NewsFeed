3577a69273fb39f299b9744e37653e7e4fb754c1
root <root@DESKTOP-VFULB7D.localdomain>
2018-11-19 20:36:15 -0500

    removed

--- server.js
+++ server.js
@@ -15,25 +15,29 @@ app.use(express.static(__dirname+'/magazine/static/'))
 
 app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   Promise.all([fetcher.popular(), fetcher.top("home"), fetcher.top("science"), fetcher.top("travel"), fetcher.top("sports"), fetcher.top("technology")]).then(function(values) {
   values.map(function(arr, index){
-    return arr.map(function(val){
-      val.byline = _.startCase(_.toLower(val.byline.substring(3)));
-      val.published_date = (new Date(val.published_date)).toLocaleDateString()
-      if(index !== 0){
-        if(val.multimedia.length === 0){
-           val.multimedia = [{url: ""},{url: ""}, {url: ""}, {url: "img/blank.png"}, {url: "img/blank.png"}]
-           }
-      }
-    })
+    if(arr != undefined){
+      return arr.map(function(val){
+        val.byline = _.startCase(_.toLower(val.byline.substring(3)));
+        val.published_date = (new Date(val.published_date)).toLocaleDateString()
+        if(index !== 0){
+          if(val.multimedia.length === 0){
+             val.multimedia = [{url: ""},{url: ""}, {url: ""}, {url: "img/blank.png"}, {url: "img/blank.png"}]
+             }
+        }
+      })
+    }else{
+      return [];
+    }
   })
 
     res.render("index", {popular: values[0], latest: values[1], science: values[2], travel: values[3], sports: values[4], technology: values[5]})
 });
-  
+
 });
 
 app.get('/category', (req, res) => {
@@ -52,7 +56,7 @@ app.get('/category', (req, res) => {
 
     res.render("category", {travel: values[0], technology: values[1], main: values[2].slice(0,values[2].length/2), category: req.query.q})
 });
-  
+
 });
 
 app.get('/post', (req, res) => {
@@ -61,24 +65,24 @@ app.get('/post', (req, res) => {
     values[1].pub_date = (new Date(values[1].pub_date)).toLocaleDateString();
     res.render("post",{article: values[0], search: values[1], technology: values[2], travel: values[3]})
   })
-  
+
 })
 
 app.post("/search", (req, res) => {
-  
+
 
   if(!req.body["Search-box"]){
     res.end();
   }
-  
+
   res.redirect("/search?q="+req.body["Search-box"])
-  
+
 })
 
 app.get("/search", (req, res) => {
-  
+
   var query = req.query.q;
-  
+
    fetcher.search(query, true).then(function(data){
      data.map(function(val){
            val.pub_date = (new Date(val.pub_date)).toLocaleDateString();
@@ -86,14 +90,14 @@ app.get("/search", (req, res) => {
         if(!val.byline){
           val.byline = "";
         }
-           
+
      })
 
      res.render("search", {query: query, results: data})
-     
+
    })
-  
-  
+
+
 })
 
 
@@ -104,4 +108,3 @@ app.get("/search", (req, res) => {
 app.listen(port, () => {
   console.log('Example app listening on port ' + port)
 })
-
--- test.js
+++
@@ -1,7 +0,0 @@
-var fetcher = require('./fetcher.js')
-
-var results = fetcher.search("https://www.nytimes.com/2018/06/24/business/media/james-wolfe-ali-watkins-leaks-reporter.html")
-
-results.then(function(data){
-  console.log(data)
-})
